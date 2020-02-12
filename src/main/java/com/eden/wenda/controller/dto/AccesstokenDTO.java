a43397c0b20c01d59d07b7d542911e41d9471b28
hbdlp <er3s@sina.com>
2020-02-12 14:59:42 +0800

    第三方登录

---
+++ src/main/java/com/eden/wenda/controller/dto/AccesstokenDTO.java
@@ -0,0 +1,8 @@
+package com.eden.wenda.controller.dto;
+
+/**
+ * DTO数据传输模型
+ * Accesstoken用于装填github第三方登录返回数据
+ */
+public class AccesstokenDTO {
+}
---
+++ src/main/java/com/eden/wenda/controller/provider/GithubProvider.java
@@ -0,0 +1,8 @@
+package com.eden.wenda.controller.provider;
+
+import org.springframework.stereotype.Component;
+
+@Component  //该注解作用是将对象自动实例化,放到一个池子里
+public class GithubProvider {
+
+}
